4497b59b3b594102949b21949746e77a2a9146da
Amady Azdaev <amady833@gmail.com>
2020-10-18 00:44:31 +0300

    Update sign.py

--- 2_statements/sign.py
+++ 2_statements/sign.py
@@ -5,11 +5,9 @@ This program determines sign of an inputted number
 """
 
 
-while True:
-    try:
-        number = int(input('Input a number: '))
-        break
-    except ValueError:
-        print('Not a number')
+try:
+    number = int(input('Input a number: '))
+except ValueError:
+    print('Not a number')
 
 print((number>0) - (number<0))
