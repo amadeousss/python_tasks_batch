3d6fbf7eb231410e4cb9cea94efa4f42b74fb74a
amadeousss <amady833@gmail.com>
2020-10-20 23:47:37 +0300

    Fixed error handling.

--- 3_calculations/bun.py
+++ 3_calculations/bun.py
@@ -10,10 +10,9 @@ n amount of buns.
 try:
     dollars = int(input('Dollars: '))
     cents = int(input('Cents (1-99): '))
-    if cents < 1 or cents > 99:
-        print('amount of cents has to be between 1 and 99')
-    else:
-        amount = int(input('How many: '))
-        print(f'{dollars*amount + cents*amount//100}.{cents*amount%100}')
 except ValueError:
     print('Input error')
+if cents < 1 or cents > 99:
+    raise Exception('Inputted amount of cents is less than 1 or more than 99')
+amount = int(input('How many: '))
+print(f'{dollars*amount + cents*amount//100}.{cents*amount%100}')
